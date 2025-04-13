425b76fb540a45fba1dc29b8409cec797f227faa
Chaotic <124513444+Volburaal@users.noreply.github.com>
2025-04-13 11:47:43 +0500

    Account Re assignment (Not fully done)

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,6 +4,23 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+const generateStudentEmail = (rollnumber) => {
+  // const campusCodes = {
+  //   F: 'cfd',
+  //   I: 'isb',
+  //   L: 'lhr',
+  //   P: 'pwr',
+  //   K: 'khi',
+  // };
+
+  const batch = rollnumber.substring(0, 2);
+  const campus = rollnumber[2].toLowerCase();
+  const studentNumber = rollnumber.substring(4);
+  
+  return `${campus}${batch}${studentNumber}@cfd.nu.edu.pk`;
+  //return `${campus}${batch}${studentNumber}@${campusCodes[campus]}.nu.edu.pk`;
+};
+
 // Register a new user
 export const register = async (req, res) => {
   try {
@@ -157,12 +174,14 @@ export const login = async (req, res) => {
   }
 };
 
-
 export const modifyUser = async (req, res) => {
   try {
     const { id } = req.params;
-    const { email, role, affiliation, password, assignedTo } = req.body;
+    let { email, role, affiliation, password, assignedTo } = req.body;
     const now = new Date();
+    if(!assignedTo){
+      assignedTo=-1
+    }
 
     const validRoles = [
       "STUDENT",
@@ -189,22 +208,180 @@ export const modifyUser = async (req, res) => {
     if (!existingUser) {
       return res.status(404).json({ message: "User not found" });
     }
+    const oldPassword = existingUser.password;
 
     let newAssignedToStudent = existingUser.assignedToStudent;
     let newAssignedToFaculty = existingUser.assignedToFaculty;
-
+    let updateEmail = ""
+    let emailBody = ""
+    let emailTitle = ""
     if (assignedTo !== undefined) {
       if (assignedTo === -1) {
         newAssignedToStudent = null;
         newAssignedToFaculty = null;
-      } else if (role === "STUDENT") {
+      }
+      else if (role === "STUDENT") {
+        const oldAssignedTo = existingUser.assignedToStudent;
+        if((oldAssignedTo !== null && assignedTo !== -1) && (oldAssignedTo === assignedTo) && (hashedPassword !== oldPassword)){
+          const student = await prisma.student.findUnique({
+            where:{id: assignedTo}
+          })
+          const roll = student.rollnumber;
+          updateEmail = generateStudentEmail(roll)
+          emailTitle = "SFC Portal Password Update";
+          emailBody = `
+            <h1 style="color:rgb(213, 238, 255); text-align:center; background-color: rgb(67, 0, 87); padding: 2%; margin:0px; border-radius: 50px 50px 0px 0px;">Password Reset</h1>
+            <div style="color: rgb(248, 199, 255); background-color: rgb(49, 49, 49); margin: 0px; padding: 5%; border-radius: 0px 0px 50px 50px;">
+              <h3 style="text-align: center;"><b>The password for ${email} has been changed</b><br></h3>
+              <h4>New Credentials for the NEST-SAM portal are provided below</h4>
+              <br>
+              <p >Username: <b>${email}</b></p>
+              <p>Password: <b>${password}</b></p>
+              <br>
+              <p>Access the portal on the provided <a href="http://59.103.246.24:3000" style="color: deeppink;">IP</a> (59.103.246.24:3000). Please note that the portal is only accesible on university internet</p>
+            </div>
+          `;
+        }
+        if ((oldAssignedTo === null && assignedTo !== -1)||(oldAssignedTo !== null && (oldAssignedTo !== assignedTo))) {
+          if(!password){
+            return res.status(400).json({message: "Password must be reset when account is re-assigned"})
+          }
+          const student = await prisma.student.findUnique({
+            where:{id: assignedTo}
+          })
+          const roll = student.rollnumber;
+          updateEmail = generateStudentEmail(roll)
+          emailTitle = "SFC Portal Account Assignment";
+          emailBody = `
+            <h1 style="color:rgb(213, 238, 255); text-align:center; background-color: rgb(67, 0, 87); padding: 2%; margin:0px; border-radius: 50px 50px 0px 0px;">Account Allocated</h1>
+            <div style="color: rgb(248, 199, 255); background-color: rgb(49, 49, 49); margin: 0px; padding: 5%; border-radius: 0px 0px 50px 50px;">
+                <h3 style="text-align: center;"><b>The account ${email} has been allocated to you</b><br></h3>
+                <h4>Your responsibilities include:</h4>
+                <ul>
+                  <li>Creating and tracking proposals on behalf of your society</li>
+                  <li>Managing all society member records</li>
+                  <li>Scheduling meetings</li>
+                  <li>Updating meeting details such as minutes and attendance after meetings</li>
+                </ul>
+                <h4>Credentials for the NEST-SAM portal are provided below</h4>
+                <br>
+                <p >Username: <b>${email}</b></p>
+                <p>Password: <b>${password}</b></p>
+                <br>
+                <p>Access the portal on the provided <a href="http://59.103.246.24:3000" style="color: deeppink;">IP</a> (59.103.246.24:3000). Please note that the portal is only accesible on university internet</p>
+              </div>
+          `;
+        }
         newAssignedToStudent = assignedTo;
         newAssignedToFaculty = null;
-      } else {
+      }
+      else {
+        const oldAssignedTo = existingUser.assignedToFaculty;
+        if((oldAssignedTo !== null && assignedTo !== -1) && (oldAssignedTo === assignedTo) && (hashedPassword !== oldPassword)){
+          const faculty = await prisma.faculty.findUnique({
+            where:{id: assignedTo}
+          })
+          updateEmail = faculty.email;
+          emailTitle = "SFC Portal Password Update";
+          emailBody = `
+            <h1 style="color:rgb(213, 238, 255); text-align:center; background-color: rgb(67, 0, 87); padding: 2%; margin:0px; border-radius: 50px 50px 0px 0px;">Password Reset</h1>
+            <div style="color: rgb(248, 199, 255); background-color: rgb(49, 49, 49); margin: 0px; padding: 5%; border-radius: 0px 0px 50px 50px;">
+              <h3 style="text-align: center;"><b>The password for ${email} has been changed</b><br></h3>
+              <h4>New Credentials for the NEST-SAM portal are provided below</h4>
+              <br>
+              <p >Username: <b>${email}</b></p>
+              <p>Password: <b>${password}</b></p>
+              <br>
+              <p>Access the portal on the provided <a href="http://59.103.246.24:3000" style="color: deeppink;">IP</a> (59.103.246.24:3000). Please note that the portal is only accesible on university internet</p>
+            </div>
+          `;
+        }
+        if ((oldAssignedTo === null && assignedTo !== -1)||(oldAssignedTo !== null && (oldAssignedTo !== assignedTo))) {
+          if(!password){
+            return res.status(400).json({message: "Password must be reset when account is re-assigned"})
+          }
+          const faculty = await prisma.faculty.findUnique({
+            where:{id: assignedTo}
+          })
+          let responsibilities = `
+            <li>It seems that there are no responsibilities to dish out</li>
+          `;
+          if (role == "MENTOR") {
+            responsibilities = `
+              <li>Reviewing proposals made by your society</li>
+              <li>Managing all society member records</li>
+              <li>Scheduling meetings</li>
+              <li>Updating meeting details such as minutes and attendance after meetings</li>
+            `;
+          }
+          else if (role == "STUDENT_AFFAIRS") {
+            responsibilities = `
+              <li>Reviewing proposals made by societies</li>
+              <li>Managing student, faculty and society records</li>
+              <li>Managing society leadership such as mentor and co-mentor</li>
+              <li>Creating proposals for events not affiliated to any society</li>
+              <li>Managing society account assignments</li>
+              <li style="color: rgb(255, 0, 191); font-weight: bold; list-style-type: none;">Proposals shall be available for reivew once reviewed by the respective society mentor</li>
+            `;
+          }
+          else if (role == "DIRECTOR") {
+            responsibilities = `
+              <li>Reviewing proposals made by societies</li>
+              <li style="color: rgb(255, 0, 191); font-weight: bold; list-style-type: none;">Proposals shall be available for reivew once reviewed by the student affairs incahrge</li>
+            `;
+          }
+          else if (role == "FINANCE_MANAGER") {
+            responsibilities = `
+              <li>Reviewing proposals made by societies and allocating budget if required</li>
+              <li style="color: rgb(255, 0, 191); font-weight: bold; list-style-type: none;">Proposals shall be available for reivew once reviewed by the director</li>
+            `;
+          }
+          updateEmail = faculty.email;
+          emailTitle = "SFC Portal Account Assignment";
+          emailBody = `
+            <h1 style="color:rgb(213, 238, 255); text-align:center; background-color: rgb(67, 0, 87); padding: 2%; margin:0px; border-radius: 50px 50px 0px 0px;">Account Allocated</h1>
+            <div style="color: rgb(248, 199, 255); background-color: rgb(49, 49, 49); margin: 0px; padding: 5%; border-radius: 0px 0px 50px 50px;">
+                <h3 style="text-align: center;"><b>The account ${email} has been allocated to you</b><br></h3>
+                <h4>Your responsibilities include:</h4>
+                <ul>
+                  ${responsibilities}
+                </ul>
+                <h4>Credentials for the NEST-SAM portal are provided below</h4>
+                <br>
+                <p >Username: <b>${email}</b></p>
+                <p>Password: <b>${password}</b></p>
+                <br>
+                <p>Access the portal on the provided <a href="http://59.103.246.24:3000" style="color: deeppink;">IP</a> (59.103.246.24:3000). Please note that the portal is only accesible on university internet</p>
+              </div>
+          `;
+        }
         newAssignedToStudent = null;
         newAssignedToFaculty = assignedTo;
       }
     }
+
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.SENDER_ADDRESS,
+        pass: process.env.EMAIL_APP_PASSWORD,
+      },
+    });
+    const mailOptions = {
+      from: process.env.SENDER_ADDRESS,
+      to: '',
+      bcc: [updateEmail],
+      subject: emailTitle,
+      html: emailBody,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error('Error sending email:', error);
+        return res.status(500).json({ error: 'Error sending email' });
+      }
+      return res.status(200).json({ message: 'Update Email Sent'});
+    });
 
     const updatedUser = await prisma.$transaction(async (prisma) => {
       if (role && role !== existingUser.role) {
